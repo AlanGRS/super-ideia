08be3eacc97b537ed3636901101a149bea11308c
AlanGRS <alan.grs000@gmail.com>
2022-02-19 23:51:22 -0300

    teste de next link

--- pages/sobre.js
+++ pages/sobre.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 function Sobre() {
     return (
         <div>
-            <h1>Página sobre</h1>
+            <h1>Página Sobre</h1>
 
             <link href='/'>
                 <a href="/">Acessar página inicial</a>
