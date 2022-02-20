dbdaf2526b967a26ef7b083e660650e2f1091458
AlanGRS <alan.grs000@gmail.com>
2022-02-20 00:01:31 -0300

    teste next link

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 function Home() {
     return (
         <div>
-            <h1>Página Inicial</h1>
+            <h1>Home</h1>
 
             <link href='/sobre'>
                 <a>Acessar página sobre</a>
